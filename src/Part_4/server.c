ac02ccaa2344a0c34c061052761aa88f21d72b97
Valeriya Vasyutova <126957522+Sohorn@users.noreply.github.com>
2024-11-11 15:18:45 +0300

    Add files via upload

---
+++ src/Part_4/server.c
@@ -0,0 +1,11 @@
+#include "fcgi_stdio.h"
+#include <stdio.h>
+
+int main(void) {
+
+  while(FCGI_Accept() >= 0) {
+    printf("Content-type: text/html\r\nStatus: 200 OK\r\n\r\n<h2>Hello World!<h2>");
+  }
+
+  return 0;
+}
